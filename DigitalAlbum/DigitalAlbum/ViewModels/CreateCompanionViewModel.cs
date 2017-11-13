9286fa8ac14c907537836ab440e8b4b95b887312
Rabbit <venkatasivaprasadreddypulagam@gmail.com>
2017-11-13 18:35:25 +0530

    Commiting changes

---
+++ DigitalAlbum/DigitalAlbum/ViewModels/CreateCompanionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DigitalAlbum
+{
+    public class CreateCompanionViewModel : BaseViewModel
+    {
+        public INavigation Navigation { get; set; }
+        public CreateCompanionViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+    }
+}
---
+++ DigitalAlbum/DigitalAlbum/Views/CreateCompanion.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DigitalAlbum
+{
+    public partial class CreateCompanion : ContentPage
+    {
+        public CreateCompanion()
+        {
+            InitializeComponent();
+            BindingContext = new CreateCompanionViewModel(Navigation);
+        }
+    }
+}
